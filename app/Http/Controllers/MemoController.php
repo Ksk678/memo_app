bed1bf162fbcec711e14671698f93440487721d4
Ksk678 <yagishit59@gmail.com>
2025-01-19 14:08:17 +0900

    一覧機能作成

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -7,6 +7,13 @@ use Illuminate\Http\Request;
 
 class MemoController extends Controller
 {
+    //全件取得
+    public function index()
+    {
+    $memos = Memo::all();
+    return view("memos.index", ["memos" => $memos]);
+        }
+
     // showページへ移動
     public function show($id) {
         $memo = Memo::find($id);
