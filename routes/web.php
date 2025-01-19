261947eaa6befa92e39fa12fc0fd5cbc61df4976
Ksk678 <yagishit59@gmail.com>
2025-01-19 11:28:51 +0900

    プロジェクトの準備

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\MemoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::resource("memos",MemoController::class);
