368d3d875b66b13512713ef348eec7489038d2b9
Ksk678 <yagishit59@gmail.com>
2025-01-19 17:38:07 +0900

    削除ボタン追加

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -23,8 +23,18 @@ class MemoController extends Controller
     public function store(Request $request)
     {
         //インスタンスの作成
-    }
+        $memo = new Memo();
 
+        //値の用意
+        $memo->title = $request->title;
+        $memo->body = $request->body;
+
+        //インスタンスに値を設定して保存
+        $memo->save();
+
+        //登録したらindexに戻る
+        return redirect(route("memos.index"));
+    }
     // showページへ移動
     public function show($id)
     {
@@ -50,7 +60,15 @@ class MemoController extends Controller
         //インスタンスに値を設定して保存
         $memo->save();
 
-        //登録したらindexに戻る
+        //更新したらindexに戻る
         return redirect(route("memos.index"));
+    }
+
+    public function destroy($id)
+    {
+        $memo = memo::find($id);
+        $memo->delete();
+
+        return redirect(route ("memos.index"));
     }
 }
